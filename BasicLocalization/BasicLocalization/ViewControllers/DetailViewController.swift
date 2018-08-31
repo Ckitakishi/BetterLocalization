4a03172e0977732fd2361d72e871c1c2615c8783
Ckitakishi <chenyuhan43@gmail.com>
2018-08-31 19:15:54 +0900

    Fix bugs

--- BasicLocalization/BasicLocalization/ViewControllers/DetailViewController.swift
+++ BasicLocalization/BasicLocalization/ViewControllers/DetailViewController.swift
@@ -28,8 +28,6 @@ class DetailViewController: UIViewController {
         super.viewDidLoad()
 
         detailTableView.dataSource = self
-        detailTableView.tableHeaderView = headerView
-        detailTableView.sectionHeaderHeight = UITableView.automaticDimension
         
         nameLabel.text = roomDataSource?.name
         
@@ -41,6 +39,8 @@ class DetailViewController: UIViewController {
             fatalError("..!")
         }
         detailDataSource = details
+        
+        detailTableView.layoutTableHeaderView()
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -69,3 +69,35 @@ extension DetailViewController: UITableViewDataSource {
     }
 }
 
+extension UITableView {
+    
+    // Reference: http://www.codeido.com/2018/02/dynamic-uitableview-header-view-height-using-auto-layout/
+    func layoutTableHeaderView() {
+        
+        guard let headerView = tableHeaderView else {
+            return
+            
+        }
+        headerView.translatesAutoresizingMaskIntoConstraints = false
+        
+        let headerWidth = headerView.bounds.size.width;
+        let temporaryWidthConstraints = NSLayoutConstraint.constraints(withVisualFormat: "[headerView(width)]", options: NSLayoutConstraint.FormatOptions(rawValue: UInt(0)), metrics: ["width": headerWidth], views: ["headerView": headerView])
+        
+        headerView.addConstraints(temporaryWidthConstraints)
+        
+        headerView.setNeedsLayout()
+        headerView.layoutIfNeeded()
+        
+        let headerSize = headerView.systemLayoutSizeFitting(UIView.layoutFittingCompressedSize)
+        let height = headerSize.height
+        var frame = headerView.frame
+        
+        frame.size.height = height
+        headerView.frame = frame
+        
+        tableHeaderView = headerView
+        
+        headerView.removeConstraints(temporaryWidthConstraints)
+        headerView.translatesAutoresizingMaskIntoConstraints = true
+    }
+}
